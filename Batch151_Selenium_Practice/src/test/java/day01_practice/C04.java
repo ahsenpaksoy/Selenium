14a6c42ad59c5f87d99a2c7e848fa3e2594bced1
ahsenyildirim <ahsenpaksoy@gmail.com>
2023-06-04 20:18:02 +0300

    c05 and c04 update

--- Batch151_Selenium_Practice/src/test/java/day01_practice/C04.java
+++ Batch151_Selenium_Practice/src/test/java/day01_practice/C04.java
@@ -41,51 +41,13 @@ public class C04 {
 
         // Title'in "MEDUNNA" kelimesini icerip icermedigini kontrol ediniz.
         if(driver.getTitle().contains("MEDUNNA")){
-            
-        }
+            System.out.println("Title contains MEDUNNA");
+        }else System.out.println("Title does not contain MEDUNNA");
 
         // URL'in "medunna" kelimesini icerip icermedigini kontrol ediniz
-        // teknosa adresine geri donunuz
-        // Sayfayı yenileyiniz
-        // medunna adresine ilerleyiniz
-        // Sayfayı yenileyiniz
-        // pencereyi kapat
-
-        // Title'in "Teknoloji" kelimesini icerip icermedigini kontrol ediniz
-        if (driver.getTitle().contains("Teknoloji")){
-            System.out.println("pages contains technology");
-        }else {
-            System.out.println("pages does not contain technology");
-        }
-
-        // URL'in "teknosa" kelimesini icerip icermedigini kontrol ediniz
-        if (driver.getCurrentUrl().contains("teknosa")){
-            System.out.println("Pages contains teknosa");
-        }else {
-            System.out.println("pages does not contain teknosa");
-        }
-
-        // "https://medunna.com/" adresine gidiniz
-        Thread.sleep(1000);
-        driver.get("https://medunna.com/");
-
-        // Sayfanin Title'ini ve URL'ini aliniz ve yazdiriniz
-        System.out.println("Meduna pages tittle: "+driver.getTitle());
-        System.out.println("Medunna pages URL: "+driver.getCurrentUrl());
-
-        // Title'in "MEDUNNA" kelimesini icerip icermedigini kontrol ediniz.
-        if(driver.getTitle().contains("Medunaa")){
-            System.out.println("Meduna pages contains Medunna");
-        }else {
-            System.out.println("Medunna does not contains Medunna");
-        }
-
-        // URL'in "medunna" kelimesini icerip icermedigini kontrol ediniz
-        if (driver.getCurrentUrl().contains("medunna")){
-            System.out.println("Medunna pages contains Medunna");
-        }else {
-            System.out.println("Medunna does not contains Medunna");
-        }
+        if(driver.getCurrentUrl().contains("medunna")){
+            System.out.println("Url contains medunna");
+        }else System.out.println("Url does not contain medunna");
 
         // teknosa adresine geri donunuz
         Thread.sleep(1000);
