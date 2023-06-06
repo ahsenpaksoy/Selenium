381367a60745a8176d8da3d5494d0dccf176fa7c
ahsenyildirim <ahsenpaksoy@gmail.com>
2023-06-06 22:58:34 +0300

    beforeClass-afterClass-assertion

--- Batch151MavenJUnit/src/test/java/techproed/day07_MavenUnit/C03_BeforeAfter.java
+++ Batch151MavenJUnit/src/test/java/techproed/day07_MavenUnit/C03_BeforeAfter.java
@@ -54,6 +54,14 @@ public class C03_BeforeAfter {
             -“radio buttons” elementlerini locate edin
             -Secili degilse cinsiyet butonundan size uygun olani secin
 
+            *** Aşağıdaki Task'i Junit framework'u ile yapınız
+            - Before methodu ile drive'i olusturup ayarlamalari yapiniz
+            - ebay sayfasına gidiniz
+            - electronics bölümüne tıklayınız
+            - Genişliği 225 ve Uzunluğu 225 olan resimlerin hepsine tıklayalım
+            - Her sayfanın sayfa başlığını yazdıralım
+            - After methodu ile sayfayı kapatalım
+
          */
 
 
