67aa617c65945f58ca5d1674f6079bd4a45bbf8a
ahsenyildirim <ahsenpaksoy@gmail.com>
2023-08-14 13:15:07 +0300

    API adding new classes

---
+++ B151_API/src/test/java/get_requests/Get06.java
@@ -0,0 +1,99 @@
+package get_requests;
+
+import base_urls.HerokuAppBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+public class Get06 extends HerokuAppBaseUrl {
+    /*
+      Given
+        https://restful-booker.herokuapp.com/booking/22
+    When
+        User send a GET request to the URL
+    Then
+        HTTP Status Code should be 200
+    And
+        Response content type is “application/json”
+    And
+        Response body should be like;
+      {
+        "firstname": "John",
+        "lastname": "Smith",
+        "totalprice": 111,
+        "depositpaid": true,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+     */
+    @Test
+    public void Get(){
+
+        spec.pathParams("first","booking"
+                ,"second",22);
+
+        Response response = given()
+                .spec(spec)
+                .when()
+                .get("{first}/{second}");
+
+        response.prettyPrint();
+
+        //1. Yol
+        response
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("firstname",equalTo("John"),
+                        "lastname",equalTo("Smith"),
+                        "totalprice",equalTo(111),
+                        "depositpaid",equalTo(true),
+                        "bookingdates.checkin",equalTo("2018-01-01"),
+                        "bookingdates.checkout",equalTo("2019-01-01"),
+                        "additionalneeds",equalTo("Breakfast")
+                );
+
+        //2. Yol: Response data içerrisindeki değerlere ulaşmak için Jsonpath kullanılır
+        JsonPath json = response.jsonPath();
+        // System.out.println(json.getString("firstname"));//Jane
+//        System.out.println(json.getInt("totalprice"));//111
+//        System.out.println(json.getBoolean("depositpaid"));//true
+        assertEquals("John",json.getString("firstname"));
+        assertEquals("Smith",json.getString("lastname"));
+        assertEquals(111,json.getInt("totalprice"));
+        assertEquals(true,json.getBoolean("depositpaid"));
+        assertEquals("2018-01-01",json.getString("bookingdates.checkin"));
+        assertEquals("2019-01-01",json.getString("bookingdates.checkout"));
+        assertEquals("Breakfast",json.getString("additionalneeds"));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
